d096aeea1d0bc0be8dd699a8de46658ccecbb13c
Ziv <ziv.perry@zoominfo.com>
2023-01-14 21:03:42 +0200

    build

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
 import './style.css'
 
-function append(object: unknown) {
-    console.log(object);
+type V = number | undefined;
+
+function append(x: V, y: V, z: V) {
     const pre = document.querySelector('pre') as HTMLElement;
-    pre.innerHTML += '\n\n' + JSON.stringify(object);
+    pre.innerHTML = `x = ${x}\ny = ${y}\nz = ${z}`;
 }
 
 document.querySelector('button')?.addEventListener('click', () => {
-    navigator.permissions
-        .query({name: "geolocation"})
-        .then(status => {
-            append({name: status.name, state: status.state});
-            navigator.geolocation.getCurrentPosition(
-                ({coords, timestamp}) => append({coords, timestamp}),
-                ({message, code}) => append({error: {message, code}})
-            )
+    // @ts-ignore
+    navigator.permissions.query({name: "accelerometer"})
+        .then(() => {
+            const acc = new Accelerometer({frequency: 60});
+            acc.addEventListener('reading', () => {
+                const {x, y, z} = acc;
+                append(x, y, z);
+            });
         });
 });
