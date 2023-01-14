0319afea66518491cdeb7eca8867a71cd3335f7a
Ziv <ziv.perry@zoominfo.com>
2023-01-14 20:48:47 +0200

    build

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 
 function append(object: unknown) {
+    console.log(object);
     const pre = document.querySelector('pre') as HTMLElement;
     pre.innerHTML += '\n\n' + JSON.stringify(object);
 }
@@ -9,10 +10,10 @@ document.querySelector('button')?.addEventListener('click', () => {
     navigator.permissions
         .query({name: "geolocation"})
         .then(status => {
-            append({status});
+            append({name: status.name, state: status.state});
             navigator.geolocation.getCurrentPosition(
-                (pos) => append({pos}),
-                (err) => append({err})
+                ({coords, timestamp}) => append({coords, timestamp}),
+                ({message, code}) => append({error: {message, code}})
             )
         });
 });
